91438bc99d50033ef456b2b08e89c9c7b7b376eb
DFreireF <D.FreireFernandez@gsi.de>
2023-03-15 18:42:25 +0100

    up

--- collodstex/__main__.py
+++ collodstex/__main__.py
@@ -14,15 +14,13 @@ def main():
 
     # Actions
     parser.add_argument('-l', '--log', dest = 'logLevel', choices = ['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'], default = 'INFO', help = 'Set the logging level.')
-    parser.add_argument('-o', '--outdir', type = str, nargs = '?', default = os.getcwd(), help = 'Output directory.')
 
     args = parser.parse_args()
 
     # Extra Details
     if args.logLevel: log.basicConfig(level = log.getLevelName(args.logLevel))
-    if args.outdir and args.texout: outfilepath = os.path.join(args.outdir, '')
 
-    controller(args.odsin, outfilepath + args.texout)
+    controller(args.odsin[0],  args.texout)
     
 def controller(odsin, texout = None):
     
@@ -63,14 +61,14 @@ def controller(odsin, texout = None):
         for af in affiliations:
             if af is not None:
                 afil_str = afil_str + '\\affiliation{'+af+'}\n'
-                
+        if (authors1 or authors2) is None: break
         author_str = authors1 +' '+ authors2
         names = '\\author{'+author_str+'}\n'
         alla.append(names+afil_str)
         
     if texout is None: 
         head, tail = os.path.split(odsin)
-        textout = head + '/' + tail[:-4] + '.tex'
+        texout = head + '/' + tail[:-4] + '.tex'
     with open(texout, 'w') as f:
         for row in alla:
             f.write(row)
